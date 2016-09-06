1be9777636f6902634a1635be3062f3d1d2d9b89
Nathan Leiby <nathan.leiby@clever.com>
2016-09-05 17:59:03 -0700

    detect count

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 Superset
 ====
 
+## Ideas to Explore
+
+- Template matching
+    - http://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/template_matching/template_matching.html
+	- http://docs.opencv.org/3.1.0/d4/dc6/tutorial_py_template_matching.html
+	- with scaling - http://www.pyimagesearch.com/2015/01/26/multi-scale-template-matching-using-python-opencv/
+- Shape Detection
+	- http://www.pyimagesearch.com/2016/02/08/opencv-shape-detection/
+	- http://stackoverflow.com/questions/11424002/how-to-detect-simple-geometric-shapes-using-opencv
+- Square detection
+	- http://stackoverflow.com/questions/8667818/opencv-c-obj-c-detecting-a-sheet-of-paper-square-detection
+	- http://stackoverflow.com/questions/10533233/opencv-c-obj-c-advanced-square-detection
+- Circle detection
+	- http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/hough_circle/hough_circle.html#hough-circle
+- OpenCV Python examples
+	- https://github.com/opencv/opencv/tree/master/samples/python
+
+## Setup
+
+**Python Setup**
+
+Make a virtual env.
+Install requirements.
+
+```
+mkvirtualenv superset
+pip install -r requirements.txt
+```
+
+**OpenCV2 Setup**
+
+Followed these steps http://www.learnopencv.com/install-opencv-3-on-yosemite-osx-10-10-x/
+
+Install OpenCV 2 on Mac OSX
+
+```
+brew tap homebrew/science
+brew install opencv
+```
+
+Set up so Python can import y creating a couple of symlinks.
+
+```
+ln /usr/local/lib/python2.7/site-packages/cv.py cv.py
+ln /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
+```
--- grayscale.py
+++ grayscale.py
@@ -1,5 +1,101 @@
 import numpy as np
+import numpy
 import cv2
+from matplotlib import pyplot as plt
 
-img = cv2.imgload("set1.png", cv2.IMREAD_GRAYSCALE) # load in grayscale
-cv2.imwrite('set1-grayscale.png', img)
+# Show images on a plot
+def display(imgs_defs):
+  fig = plt.figure()
+  for idx, i in enumerate(img_defs):
+    plt.subplot(4,2,idx+1)
+    plt.imshow(i['image'],cmap = 'gray')
+    plt.title(i['title']), plt.xticks([]), plt.yticks([])
+
+  # close plot if you hit any key from terminal
+  plt.draw()
+  plt.pause(1) # <-------
+  raw_input("<Hit Enter To Close>")
+  plt.close()
+
+def normalize_colors(I):
+  In = I.astype(float)
+  II = In ** 2
+  II = np.sum(II, axis=2)
+  II = np.sqrt(II)
+  II = II[..., np.newaxis]
+  II = np.ma.concatenate((II, II, II), axis=2)
+  C = np.divide(I, II)
+  C = cv2.normalize(C, alpha=0, beta=255,
+                    norm_type=cv2.NORM_MINMAX)
+  IC = np.uint8(C)
+  return IC
+
+def to_bw(img):
+  threshold = 130 # TODO: May need to adjust. 115 result in all-black
+  GRAY = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  BW = cv2.threshold(GRAY, threshold, 255, cv2.THRESH_BINARY)[1]
+  return numpy.invert(BW)
+
+def detect_count(img_bw):
+  BWt = img_bw.copy()
+  cntrs, hircy = cv2.findContours(BWt,
+                                  cv2.RETR_EXTERNAL,    # cv2.RETR_TREE,
+                                  cv2.CHAIN_APPROX_SIMPLE)
+  F = numpy.zeros(img_bw.shape, dtype=numpy.uint8)
+  MASK = numpy.zeros(img_bw.shape, dtype=numpy.uint8)
+  areas = [cv2.contourArea(cnt) for cnt in cntrs]
+  t = numpy.mean(filter(lambda x: x > 50, areas))
+  t = t * 0.9
+  MASKrect = None
+  MASKcnt = None
+  wasMasked = False
+  for idx, cnt in enumerate(cntrs):
+    if (areas[idx] > t):
+      # thickness -1 will fill the conntours
+      cv2.drawContours(F, [cnt], 0, (255), thickness=-1)
+      if (not wasMasked):
+        wasMasked = True
+        cv2.drawContours(MASK, [cnt], 0, (255), thickness=-1)
+        MASKcnt = cnt
+        MASKrect = cv2.boundingRect(cnt)
+
+  #cv2.imshow('Count', F)
+  #cv2.imshow('Mask', self.MASK)
+  cntrs, hircy = cv2.findContours(F,
+                                  cv2.RETR_EXTERNAL,
+                                  cv2.CHAIN_APPROX_SIMPLE)
+
+  amount = len(cntrs)
+  return amount
+
+img = cv2.imread("images/single-card/green-oval.png") # load in color
+edges = cv2.Canny(img,100,200)
+normalized = normalize_colors(img)
+normalized_edges = cv2.Canny(normalized,100,200)
+bw = to_bw(img)
+
+
+# non images
+count = detect_count(bw)
+print "Count = ", count
+
+img_defs = [
+  dict(title='original', image=img),
+  dict(title='edges', image=edges),
+  dict(title='normalized', image=normalized),
+  dict(title='normalized edges', image=normalized_edges),
+  dict(title='bw', image=bw),
+]
+display(img_defs)
+
+## only closes plot from GUI
+#plt.show()
+
+## Open CV image display
+# win = cv2.namedWindow('normalized')
+# cv2.imshow('normalized', normalized)
+# k = cv2.waitKey(0)
+# if k == 27:         # wait for ESC key to exit
+  # cv2.destroyAllWindows()
+
+
