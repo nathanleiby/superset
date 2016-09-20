71726d5d8ca5e465af3f675ee72485678983a39f
Nathan Leiby <nathan.leiby@clever.com>
2016-09-20 00:09:08 -0700

    server - experiment with file upload

---
+++ server.py
@@ -0,0 +1,42 @@
+"""
+From: https://gist.github.com/dAnjou/2874714
+"""
+
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/tmp/'
+ALLOWED_EXTENSIONS = set(['txt','jpg'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        print("POST")
+        f = request.files['file']
+        print("FILE", f)
+        if f and allowed_file(f.filename):
+            filename = secure_filename(f.filename)
+            print('here')
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('index'))
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    <p>%s</p>
+    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5001, debug=True)
